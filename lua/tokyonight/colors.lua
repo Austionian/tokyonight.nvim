e7c5f1517f97d78a3efb786f22075a7c900adb57
Austin Rooks <Austionian@users.noreply.github.com>
2022-08-18 22:19:05 -0500

    try this instead

--- lua/tokyonight/colors.lua
+++ lua/tokyonight/colors.lua
@@ -13,7 +13,7 @@ function M.setup(config)
 
   colors = {
     none = "NONE",
-    bg_dark = "#000000",
+    bg_dark = "#1f2335",
     bg = "#24283b",
     bg_highlight = "#292e42",
     terminal_black = "#414868",
@@ -46,8 +46,8 @@ function M.setup(config)
     gitSigns = { add = "#164846", change = "#394b70", delete = "#823c41" },
   }
   if config.style == "night" or config.style == "day" or vim.o.background == "light" then
-    colors.bg = "#1a1b26"
-    colors.bg_dark = "#000000"
+    colors.bg = "#000000"
+    colors.bg_dark = "#16161e"
   end
   util.bg = colors.bg
   util.day_brightness = config.dayBrightness
